64d02782343e9a9b3ce7a20c888a388efb338a17
Jessé Bruno Correia <jesse.bcorreia@gmail.com>
2025-08-17 01:37:59 -0300

    cria a classe utilitária ConverterDados

---
+++ src/main/java/controlador/utils/ConverterDados.java
@@ -0,0 +1,30 @@
+package controlador.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class ConverterDados {
+    public static Long stringParaLong(String str) {
+        if (str == null || str.isEmpty())
+            return null;
+
+        try {
+            return Long.parseLong(str);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    public static LocalDateTime stringParaLocalDateTime(String str) {
+        if (str == null || str.isEmpty())
+            return null;
+
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+        try {
+            return LocalDateTime.parse(str, formato);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
+    }
+}
